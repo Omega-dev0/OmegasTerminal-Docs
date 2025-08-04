c87625cf2e69bdbe10d785ff2fdea4f630786035
Omega-dev0 <47953064+Omega-dev0@users.noreply.github.com>
2025-08-04 19:09:42 +0100

    metatags v2

---
+++ src/components/Metatags/index.js
@@ -0,0 +1,18 @@
+export default function Metatags() {
+    return (
+        <>
+            <title>Omega's Terminal documentation</title>
+            <meta name="description" content="Guides and addons list for omega's clanning terminal" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://terminal-doc.omegadev.xyz/" />
+            <meta property="og:title" content="Omega's Terminal documentation" />
+            <meta property="og:description" content="Guides and addons list for omega's clanning terminal" />
+            <meta property="og:image" content="https://terminal-docs.omegadev.xyz/img/meta.png" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:url" content="https://terminal-doc.omegadev.xyz/" />
+            <meta name="twitter:title" content="Omega's Terminal documentation" />
+            <meta name="twitter:description" content="Guides and addons list for omega's clanning terminal" />
+            <meta name="twitter:image" content="https://terminal-docs.omegadev.xyz/img/meta.png" />
+        </>
+    );
+};
